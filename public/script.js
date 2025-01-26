c7ac0b3e01cbf6853151f30541a98cafbff182ca
be3beke <clientservices665@gmail.com>
2025-01-26 19:56:11 +0100

    Delete public/script.js

--- public/script.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello from script.js!');
-
-// Example: Add interactivity to the page
-document.querySelector('h1').addEventListener('click', () => {
-  alert('You clicked the heading!');
-});
